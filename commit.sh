b18da2a83d9aacf82255acca5115345bacfad0e5
dgpinheiro <dgpinheiro@gmail.com>
2014-05-27 19:51:49 -0300

    same as repair-ends.pl

---
+++ commit.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+#              INGLÊS/ENGLISH
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  http://www.gnu.org/copyleft/gpl.html
+#
+#
+#             PORTUGUÊS/PORTUGUESE
+#  Este programa é distribuído na expectativa de ser útil aos seus
+#  usuários, porém NÃO TEM NENHUMA GARANTIA, EXPLÍCITAS OU IMPLÍCITAS,
+#  COMERCIAIS OU DE ATENDIMENTO A UMA DETERMINADA FINALIDADE.  Consulte
+#  a Licença Pública Geral GNU para maiores detalhes.
+#  http://www.gnu.org/copyleft/gpl.html
+#
+#  Copyright (C) 2012  Universidade de São Paulo
+#
+#  Universidade de São Paulo
+#  Laboratório de Biologia do Desenvolvimento de Abelhas
+#  Núcleo de Bioinformática (LBDA-BioInfo)
+#
+#  Daniel Guariz Pinheiro
+#  dgpinheiro@gmail.com
+#  http://zulu.fmrp.usp.br/bioinfo 
+#
+
+git add .
+git commit -m "New file" file.txt
+git push origin master
+
