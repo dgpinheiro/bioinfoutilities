b00ccf62d9261e1c15441be02b6f70d070be90fe
DANIEL GUARIZ PINHEIRO <dgpinheiro@hammer.fcav.unesp.br>
2016-01-26 00:56:26 -0200

    khmer-1.4.1 scripts with adjustments and merge-stoptags.py to merge multiple stoptags files

---
+++ find-knots.py
@@ -0,0 +1,154 @@
+#! /usr/bin/env python2
+#
+# This file is part of khmer, http://github.com/ged-lab/khmer/, and is
+# Copyright (C) Michigan State University, 2009-2015. It is licensed under
+# the three-clause BSD license; see LICENSE.
+# Contact: khmer-project@idyll.org
+#
+# pylint: disable=invalid-name,missing-docstring
+"""
+Find highly-connected k-mers.
+
+k-mers are output into a .stoptags file, for later use in partitioning.
+
+% python scripts/find-knots.py <base>
+"""
+
+import argparse
+import glob
+import os
+import textwrap
+import khmer
+import sys
+from khmer.kfile import check_input_files, check_space
+from khmer.khmer_args import info
+
+# counting hash parameters.
+DEFAULT_COUNTING_HT_SIZE = 3e6                # number of bytes
+DEFAULT_COUNTING_HT_N = 4                     # number of counting hash tables
+
+# Lump removal parameters.  Probably shouldn't be changed, but who knows?
+#
+# explanation:
+#
+# We will walk EXCURSION_DISTANCE out from each tag; if we find more than
+# EXCURSION_KMER_THRESHOLD kmers within that range, this will be a "big"
+# excursion and we will track all k-mers visited.  If we find that any
+# k-mer has been visited more than EXCURSION_KMER_COUNT_THRESHOLD times,
+# we will mark it as BAD and make it a stop tag for traversal.
+
+# don't change these!
+EXCURSION_DISTANCE = 40
+EXCURSION_KMER_THRESHOLD = 200
+EXCURSION_KMER_COUNT_THRESHOLD = 2
+# EXCURSION_KMER_COUNT_THRESHOLD=5 # -- works ok for non-diginormed data
+
+
+def get_parser():
+    epilog = """
+    Load an k-mer presence table/tagset pair created by load-graph, and a set
+    of pmap files created by partition-graph. Go through each pmap file,
+    select the largest partition in each, and do the same kind of traversal as
+    in :program:`make-initial-stoptags.py` from each of the waypoints in that
+    partition; this should identify all of the HCKs in that partition. These
+    HCKs are output to <graphbase>.stoptags after each pmap file.
+
+    Parameter choice is reasonably important. See the pipeline in
+    :doc:`partitioning-big-data` for an example run.
+
+    This script is not very scalable and may blow up memory and die horribly.
+    You should be able to use the intermediate stoptags to restart the
+    process, and if you eliminate the already-processed pmap files, you can
+    continue where you left off.
+    """
+    parser = argparse.ArgumentParser(
+        description="Find all highly connected k-mers.",
+        epilog=textwrap.dedent(epilog),
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('--n_tables', '-N', type=int,
+                        default=DEFAULT_COUNTING_HT_N,
+                        help='number of k-mer counting tables to use')
+    parser.add_argument('--min-tablesize', '-x', type=float,
+                        default=DEFAULT_COUNTING_HT_SIZE, help='lower bound on'
+                        ' the size of the k-mer counting table(s)')
+    parser.add_argument('graphbase', help='Basename for the input and output '
+                        'files.')
+    parser.add_argument('--version', action='version', version='%(prog)s ' +
+                        khmer.__version__)
+    return parser
+
+
+def main():
+    info('find-knots.py', ['graph'])
+    args = get_parser().parse_args()
+
+    graphbase = args.graphbase
+
+    # @RamRS: This might need some more work
+    infiles = [graphbase + '.pt', graphbase + '.tagset']
+    if os.path.exists(graphbase + '.stoptags'):
+        infiles.append(graphbase + '.stoptags')
+    for _ in infiles:
+        check_input_files(_, False)
+
+    check_space(infiles, False)
+
+    print >>sys.stderr, 'loading k-mer presence table %s.pt' % graphbase
+    htable = khmer.load_hashbits(graphbase + '.pt')
+
+    print >>sys.stderr, 'loading tagset %s.tagset...' % graphbase
+    htable.load_tagset(graphbase + '.tagset')
+
+    initial_stoptags = False    # @CTB regularize with make-initial
+    if os.path.exists(graphbase + '.stoptags'):
+        print >>sys.stderr, 'loading stoptags %s.stoptags' % graphbase
+        htable.load_stop_tags(graphbase + '.stoptags')
+        initial_stoptags = True
+
+    pmap_files = glob.glob(args.graphbase + '.subset.*.pmap')
+
+    print >>sys.stderr, 'loading %d pmap files (first one: %s)' % \
+        (len(pmap_files), pmap_files[0])
+    print >>sys.stderr, '---'
+    print >>sys.stderr, 'output stoptags will be in', graphbase + '.stoptags'
+    if initial_stoptags:
+        print >>sys.stderr, \
+            '(these output stoptags will include the already-loaded set)'
+    print >>sys.stderr, '---'
+
+    # create counting hash
+    ksize = htable.ksize()
+    counting = khmer.new_counting_hash(ksize, args.min_tablesize,
+                                       args.n_tables)
+
+    # load & merge
+    for index, subset_file in enumerate(pmap_files):
+        print >>sys.stderr, '<-', subset_file
+        subset = htable.load_subset_partitionmap(subset_file)
+
+        print >>sys.stderr, '** repartitioning subset... %s' % subset_file
+        htable.repartition_largest_partition(subset, counting,
+                                             EXCURSION_DISTANCE,
+                                             EXCURSION_KMER_THRESHOLD,
+                                             EXCURSION_KMER_COUNT_THRESHOLD)
+
+        print >>sys.stderr, '** merging subset... %s' % subset_file
+        htable.merge_subset(subset)
+
+        print >>sys.stderr, '** repartitioning, round 2... %s' % subset_file
+        size = htable.repartition_largest_partition(
+            None, counting, EXCURSION_DISTANCE, EXCURSION_KMER_THRESHOLD,
+            EXCURSION_KMER_COUNT_THRESHOLD)
+
+        print >>sys.stderr, '** repartitioned size:', size
+
+        print >>sys.stderr, 'saving stoptags binary'
+        htable.save_stop_tags(graphbase + '.stoptags')
+        os.rename(subset_file, subset_file + '.processed')
+        print >>sys.stderr, '(%d of %d)\n' % (index, len(pmap_files))
+
+    print >>sys.stderr, 'done!'
+
+if __name__ == '__main__':
+    main()
---
+++ sweep-reads3.py
@@ -0,0 +1,97 @@
+#! /usr/bin/env python2
+#
+# This file is part of khmer, http://github.com/ged-lab/khmer/, and is
+# Copyright (C) Michigan State University, 2009-2013. It is licensed under
+# the three-clause BSD license; see LICENSE.
+# Contact: khmer-project@idyll.org
+#
+"""
+Use a set of query reads to sweep out overlapping reads from multiple files.
+
+% python scripts/sweep-reads3.py <query1> [ <query2> ... ] <search reads>
+
+Results end up in <search reads>.sweep3.
+
+Use '-h' for parameter help.
+"""
+
+import sys
+import os.path
+import screed
+import khmer
+from khmer.khmer_args import (build_hashbits_args, DEFAULT_MIN_TABLESIZE)
+
+def output_single(r):
+    if hasattr(r, 'quality'):
+        return "@%s\n%s\n+\n%s\n" % (r.name, r.sequence, r.quality)
+    else:
+        return ">%s\n%s\n" % (r.name, r.sequence)
+
+
+def main():
+    parser = build_hashbits_args()
+    parser.add_argument('input_filenames', nargs='+')
+    parser.add_argument('read_filename')
+
+    args = parser.parse_args()
+
+    if not args.quiet:
+        if args.min_tablesize == DEFAULT_MIN_TABLESIZE:
+            print >>sys.stderr, "** WARNING: tablesize is default!  " \
+                "You absodefly want to increase this!\n** " \
+                "Please read the docs!"
+
+        print >>sys.stderr, '\nPARAMETERS:'
+        print >>sys.stderr, ' - kmer size =    %d \t\t(-k)' % args.ksize
+        print >>sys.stderr, ' - n hashes =     %d \t\t(-N)' % args.n_tables
+        print >>sys.stderr, ' - min hashsize = %-5.2g \t(-x)' % \
+            args.min_tablesize
+        print >>sys.stderr, ''
+        print >>sys.stderr, 'Estimated memory usage is %.2g bytes ' \
+            '(n_tables x min_tablesize / 8)' % (
+                args.n_tables * args.min_tablesize * len(args.input_filenames) / 8.)
+        print >>sys.stderr, '-' * 8
+
+    K = args.ksize
+    HT_SIZE = args.min_tablesize
+    N_HT = args.n_tables
+
+    inputlist = args.input_filenames
+    readsfile = args.read_filename
+
+    query_list = []
+    for n, inp_name in enumerate(inputlist):
+        # create a hashbits data structure
+        ht = khmer.new_hashbits(K, HT_SIZE, N_HT)
+
+        outfile = os.path.basename(inp_name) + '.sweep3'
+        outfp = open(outfile, 'w')
+        query_list.append((ht, outfp))
+
+    for n, inp_name in enumerate(inputlist):
+        ht = query_list[n][0]
+
+        # load contigs, connect into N partitions
+        print 'loading input reads from', inp_name
+        ht.consume_fasta(inp_name)
+
+    print 'starting sweep.'
+
+    n = 0
+    m = 0
+    for n, record in enumerate(screed.open(readsfile)):
+        if len(record.sequence) < K:
+            continue
+
+        if n % 10000 == 0:
+            print '...', n, m
+
+        for ht, outfp in query_list:
+            count = ht.get_median_count(record.sequence)[0]
+            if count:
+                outfp.write(output_single(record))
+
+if __name__ == '__main__':
+    main()
+
+# vim: set ft=python ts=4 sts=4 sw=4 et tw=79:
