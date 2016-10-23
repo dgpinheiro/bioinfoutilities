39e578e8035a941726ffa3e0e59d96bce701bff7
Daniel Guariz Pinheiro <dgpinheiro@hammer.fcav.unesp.br>
2016-10-23 18:16:22 -0400

    Rename sequences

---
+++ renameSeqs.pl
@@ -0,0 +1,179 @@
+#!/usr/bin/perl
+#
+#              INGLÊS/ENGLISH
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  http://www.gnu.org/copyleft/gpl.html
+#
+#
+#             PORTUGUÊS/PORTUGUESE
+#  Este programa é distribuído na expectativa de ser útil aos seus
+#  usuários, porém NÃO TEM NENHUMA GARANTIA, EXPLÍCITAS OU IMPLÍCITAS,
+#  COMERCIAIS OU DE ATENDIMENTO A UMA DETERMINADA FINALIDADE.  Consulte
+#  a Licença Pública Geral GNU para maiores detalhes.
+#  http://www.gnu.org/copyleft/gpl.html
+#
+#  Copyright (C) 2012  Universidade de São Paulo
+#
+#  Universidade de São Paulo
+#  Laboratório de Biologia do Desenvolvimento de Abelhas
+#  Núcleo de Bioinformática (LBDA-BioInfo)
+#
+#  Daniel Guariz Pinheiro
+#  dgpinheiro@gmail.com
+#  http://zulu.fmrp.usp.br/bioinfo
+#
+# $Id$
+
+=head1 NAME
+
+=head1 SYNOPSIS
+
+=head1 ABSTRACT
+
+=head1 DESCRIPTION
+    
+    Arguments:
+
+        -h/--help   Help
+        -l/--level  Log level [Default: FATAL] 
+            OFF
+            FATAL
+            ERROR
+            WARN
+            INFO
+            DEBUG
+            TRACE
+            ALL
+
+=head1 AUTHOR
+
+Daniel Guariz Pinheiro E<lt>dgpinheiro@gmail.comE<gt>
+
+Copyright (c) 2012 Universidade de São Paulo
+
+=head1 LICENSE
+
+GNU General Public License
+
+http://www.gnu.org/copyleft/gpl.html
+
+
+=cut
+
+use strict;
+use warnings;
+use Readonly;
+use Getopt::Long;
+
+use vars qw/$LOGGER/;
+
+INIT {
+    use Log::Log4perl qw/:easy/;
+    Log::Log4perl->easy_init($FATAL);
+    $LOGGER = Log::Log4perl->get_logger($0);
+}
+
+my ($level, $infile, $informat, $outformat, $outfile, $prefix);
+
+#Usage("Too few arguments") if $#ARGV < 0;
+GetOptions( "h|?|help" => sub { &Usage(); },
+            "l|level=s"=> \$level,
+            "i|infile=s"=>\$infile,
+            "o|outfile=s"=>\$outfile,
+            "if|informat=s"=>\$informat,
+            "of|outformat=s"=>\$outformat,
+            "p|prefix=s"=>\$prefix
+    ) or &Usage();
+
+
+if ($level) {
+    my %LEVEL = (   
+    'OFF'   =>$OFF,
+    'FATAL' =>$FATAL,
+    'ERROR' =>$ERROR,
+    'WARN'  =>$WARN,
+    'INFO'  =>$INFO,
+    'DEBUG' =>$DEBUG,
+    'TRACE' =>$TRACE,
+    'ALL'   =>$ALL);
+    $LOGGER->logdie("Wrong log level ($level). Choose one of: ".join(', ', keys %LEVEL)) unless (exists $LEVEL{$level});
+    Log::Log4perl->easy_init($LEVEL{$level});
+}
+
+use FileHandle;
+use POSIX 'isatty';
+
+$prefix||='SEQ';
+$informat||='FASTQ';
+$outformat||='FASTQ';
+
+my $fhin;
+my $fhout;
+
+if ($infile) {
+
+    $LOGGER->logdie("Wrong input file ($infile)") unless (-e $infile);
+
+    $fhin = FileHandle->new;
+    $fhin->open("<$infile");
+
+} else {
+    unless (isatty(*STDIN)) {
+        $fhin = \*STDIN;
+    } else {
+        $LOGGER->logdie("Missing input file (-i/--infile) or STDIN data");
+    }
+}
+
+if ($outfile) {
+    $fhout = FileHandle->new;
+    $fhout->open(">$outfile");
+} else {
+    $fhout = \*STDOUT;
+}
+
+use Bio::SeqIO;
+
+my $in  = Bio::SeqIO->new(-fh=>$fhin,  -format=>$informat);
+my $out = Bio::SeqIO->new(-fh=>$fhout, -format=>$outformat);
+
+my $c = 1;
+while ( my $seq = $in->next_seq() ) {
+    my $hex = sprintf("%010X", $c);
+    $seq->display_id($prefix.$hex);
+    $out->write_seq( $seq );
+    $c++;
+}
+
+# Subroutines
+
+sub Usage {
+    my ($msg) = @_;
+	Readonly my $USAGE => <<"END_USAGE";
+Daniel Guariz Pinheiro (dgpinheiro\@gmail.com)
+(c)2012 Universidade de São Paulo
+
+Usage
+
+        $0	[-h/--help] [-l/--level <LEVEL>]
+
+Argument(s)
+
+        -h      --help      Help
+        -l      --level     Log level [Default: FATAL]
+        -i      --infile    Input file [Default: STDIN]
+        -o      --outfile   Output file [Default: STDOUT]
+        -if     --informat  Input file format [Default: FASTQ]
+        -of     --outformat Output file format [Default: FASTQ]
+        -p      --prefix    Prefix [Default: SEQ]
+
+END_USAGE
+    print STDERR "\nERR: $msg\n\n" if $msg;
+    print STDERR qq[$0  ] . q[$Revision$] . qq[\n];
+	print STDERR $USAGE;
+    exit(1);
+}
+
