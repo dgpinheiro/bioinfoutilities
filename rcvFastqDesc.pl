2748f36bb7766639e4741899b2bea3badab784e7
Daniel Guariz Pinheiro <dgpinheiro@hammer.fcav.unesp.br>
2017-01-14 02:55:45 -0200

    Add multiple file loading support

--- rcvFastqDesc.pl
+++ rcvFastqDesc.pl
@@ -10,28 +10,34 @@ die "Missing infile file" unless ($infile);
 die "Wrong infile file ($infile)" unless (-e $infile);
 
 my $reffile = $ARGV[1];
-die "Missing reffile file" unless ($reffile);
-die "Wrong reffile file ($reffile)" unless (-e $reffile);
+die "Missing reference file(s)" unless ($reffile);
 
-my $seqout = Bio::SeqIO->new(-fh=>\*STDOUT, -format=>'FASTQ');
-my $seqref;
-if ($reffile =~ /\.gz$/) {
-	$seqref = Bio::SeqIO->new(-file=>"gunzip -c $reffile |", -format=>'FASTQ');
-} else {
-	$seqref = Bio::SeqIO->new(-file=>"$reffile", -format=>'FASTQ');
-}
 my %refdesc;
-while(my $seq=$seqref->next_seq()) {
-	#print $seq->display_id(),"\t",$seq->desc(),"\n";
-	$refdesc{ $seq->display_id() }->{ $seq->seq() } = $seq->desc();
+foreach my $rfile (glob("$reffile")) {
+	print STDERR "Loading $rfile ...\n";
+
+	my $seqref;
+	if ($rfile =~ /\.gz$/) {
+		$seqref = Bio::SeqIO->new(-file=>"gunzip -c $rfile |", -format=>'FASTQ');
+	} else {
+		$seqref = Bio::SeqIO->new(-file=>"cat $rfile |", -format=>'FASTQ');
+	}
+	
+	while(my $seq=$seqref->next_seq()) {
+		#print $seq->display_id(),"\t",$seq->desc(),"\n";
+		$refdesc{ $seq->display_id() }->{ $seq->seq() } = $seq->desc();
+	}
 }
+
 my $seqin;
 if ($infile =~ /\.gz/) {
 	$seqin = Bio::SeqIO->new(-file=>"gunzip -c $infile |", -format=>'FASTQ');
 } else {
 	$seqin = Bio::SeqIO->new(-file=>$infile, -format=>'FASTQ');
 }
-my %ref;
+
+my $seqout = Bio::SeqIO->new(-fh=>\*STDOUT, -format=>'FASTQ');
+
 while(my $seq=$seqin->next_seq()) {
 	#print $seq->display_id(),"\t",$refdesc{ $seq->display_id() }->{ $seq->seq() },"\n";
 	if ((exists $refdesc{ $seq->display_id() }) && (exists $refdesc{ $seq->display_id() }->{ $seq->seq() } )) {
