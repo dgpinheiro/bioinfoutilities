11cd23b7ad8378ce60dc370a36f176a36fe928ab
Daniel Guariz Pinheiro <dgpinheiro@hammer.fcav.unesp.br>
2017-01-14 02:38:16 -0200

    Add gzip support

--- rcvFastqDesc.pl
+++ rcvFastqDesc.pl
@@ -14,15 +14,23 @@ die "Missing reffile file" unless ($reffile);
 die "Wrong reffile file ($reffile)" unless (-e $reffile);
 
 my $seqout = Bio::SeqIO->new(-fh=>\*STDOUT, -format=>'FASTQ');
-
-my $seqref = Bio::SeqIO->new(-file=>$reffile, -format=>'FASTQ');
+my $seqref;
+if ($reffile =~ /\.gz$/) {
+	$seqref = Bio::SeqIO->new(-file=>"gunzip -c $reffile |", -format=>'FASTQ');
+} else {
+	$seqref = Bio::SeqIO->new(-file=>"$reffile", -format=>'FASTQ');
+}
 my %refdesc;
 while(my $seq=$seqref->next_seq()) {
 	#print $seq->display_id(),"\t",$seq->desc(),"\n";
 	$refdesc{ $seq->display_id() }->{ $seq->seq() } = $seq->desc();
 }
-
-my $seqin = Bio::SeqIO->new(-file=>$infile, -format=>'FASTQ');
+my $seqin;
+if ($infile =~ /\.gz/) {
+	$seqin = Bio::SeqIO->new(-file=>"gunzip -c $infile |", -format=>'FASTQ');
+} else {
+	$seqin = Bio::SeqIO->new(-file=>$infile, -format=>'FASTQ');
+}
 my %ref;
 while(my $seq=$seqin->next_seq()) {
 	#print $seq->display_id(),"\t",$refdesc{ $seq->display_id() }->{ $seq->seq() },"\n";
