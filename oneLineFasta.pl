8d8cdbefc08406015c2676addbf9f4926717e1b9
dgpinheiro <dgpinheiro@gmail.com>
2019-09-26 18:18:21 -0300

    added script to reformat sequences to one line in fasta file

---
+++ oneLineFasta.pl
@@ -0,0 +1,136 @@
+#!/usr/bin/env perl
+#
+#              INGLÊS/ENGLISH
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  http://www.gnu.org/copyleft/gpl.html
+#
+#
+#             PORTUGUÊS/PORTUGUESE
+#  Este programa é distribuído na expectativa de ser útil aos seus
+#  usuários, porém NÃO TEM NENHUMA GARANTIA, EXPLÍCITAS OU IMPLÍCITAS,
+#  COMERCIAIS OU DE ATENDIMENTO A UMA DETERMINADA FINALIDADE.  Consulte
+#  a Licença Pública Geral GNU para maiores detalhes.
+#  http://www.gnu.org/copyleft/gpl.html
+#
+#  Copyright (C) 2019  Universidade Estadual Paulista "Júlio de Mesquita Filho"
+#
+#  Universidade Estadual Paulista "Júlio de Mesquita Filho" (UNESP)
+#  Faculdade de Ciências Agrárias e Veterinárias (FCAV)
+#  Laboratório de Bioinformática (LB)
+#
+#  Daniel Guariz Pinheiro
+#  dgpinheiro@gmail.com
+#  http://www.fcav.unesp.br
+#
+# $Id$
+
+=head1 NAME
+
+=head1 SYNOPSIS
+
+=head1 ABSTRACT
+
+=head1 DESCRIPTION
+    
+    Arguments:
+
+        -h/--help   Help
+        -l/--level  Log level [Default: FATAL] 
+            OFF
+            FATAL
+            ERROR
+            WARN
+            INFO
+            DEBUG
+            TRACE
+            ALL
+
+=head1 AUTHOR
+
+Daniel Guariz Pinheiro E<lt>dgpinheiro@gmail.comE<gt>
+
+Copyright (C) 2019 Universidade Estadual Paulista "Júlio de Mesquita Filho"
+
+=head1 LICENSE
+
+GNU General Public License
+
+http://www.gnu.org/copyleft/gpl.html
+
+
+=cut
+
+use strict;
+use warnings;
+use Readonly;
+use Getopt::Long;
+
+use vars qw/$LOGGER/;
+
+INIT {
+    use Log::Log4perl qw/:easy/;
+    Log::Log4perl->easy_init($FATAL);
+    $LOGGER = Log::Log4perl->get_logger($0);
+}
+
+my ($level, $infile);
+
+Usage("Too few arguments") if $#ARGV < 0;
+GetOptions( "h|?|help" => sub { &Usage(); },
+            "l|level=s"=> \$level,
+            "i|infile=s"=>\$infile
+    ) or &Usage();
+
+
+if ($level) {
+    my %LEVEL = (   
+    'OFF'   =>$OFF,
+    'FATAL' =>$FATAL,
+    'ERROR' =>$ERROR,
+    'WARN'  =>$WARN,
+    'INFO'  =>$INFO,
+    'DEBUG' =>$DEBUG,
+    'TRACE' =>$TRACE,
+    'ALL'   =>$ALL);
+    $LOGGER->logdie("Wrong log level ($level). Choose one of: ".join(', ', keys %LEVEL)) unless (exists $LEVEL{$level});
+    Log::Log4perl->easy_init($LEVEL{$level});
+}
+
+use Bio::SeqIO;
+
+my $seqin = Bio::SeqIO->new(-file=>$infile, -format=>"FASTA");
+my $seqout = Bio::SeqIO->new(-fh=>\*STDOUT,-format=>"FASTA", -width=>1000000); 
+
+while (my $seq=$seqin->next_seq() ) { 
+    $seqout->write_seq($seq); 
+}
+
+
+# Subroutines
+
+sub Usage {
+    my ($msg) = @_;
+	Readonly my $USAGE => <<"END_USAGE";
+Daniel Guariz Pinheiro (dgpinheiro\@gmail.com)
+(c)2019 Universidade Estadual Paulista "Júlio de Mesquita Filho"
+
+Usage
+
+        $0	[-h/--help] [-l/--level <LEVEL>]
+
+Argument(s)
+
+        -h      --help      Help
+        -l      --level     Log level [Default: FATAL]
+        -i      --infile    Input fasta file
+
+END_USAGE
+    print STDERR "\nERR: $msg\n\n" if $msg;
+    print STDERR qq[$0  ] . q[$Revision$] . qq[\n];
+	print STDERR $USAGE;
+    exit(1);
+}
+
