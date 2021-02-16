b5bbdb0392275763f4101f1e6fa08d32ecf0d5cd
dgpinheiro <dgpinheiro@gmail.com>
2021-02-16 11:02:56 -0200

    Added replace_merged_taxid.pl

---
+++ replace_merged_taxid.pl
@@ -0,0 +1,189 @@
+#!/usr/bin/env perl
+#
+#              INGLÊS/ENGLISH
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  http://www.gnu.org/copyleft/gpl.html
+#
+#
+#             PORTUGUÊS/PORTUGUESE
+#  Este programa é distribuído na expectativa de ser útil aos seus
+#  usuários, porém NÃO TEM NENHUMA GARANTIA, EXPLÍCITAS OU IMPLÍCITAS,
+#  COMERCIAIS OU DE ATENDIMENTO A UMA DETERMINADA FINALIDADE.  Consulte
+#  a Licença Pública Geral GNU para maiores detalhes.
+#  http://www.gnu.org/copyleft/gpl.html
+#
+#  Copyright (C) 2019  Universidade Estadual Paulista "Júlio de Mesquita Filho"
+#
+#  Universidade Estadual Paulista "Júlio de Mesquita Filho" (UNESP)
+#  Faculdade de Ciências Agrárias e Veterinárias (FCAV)
+#  Laboratório de Bioinformática (LB)
+#
+#  Daniel Guariz Pinheiro
+#  dgpinheiro@gmail.com
+#  http://www.fcav.unesp.br
+#
+# $Id$
+
+=head1 NAME
+
+=head1 SYNOPSIS
+
+=head1 ABSTRACT
+
+=head1 DESCRIPTION
+    
+    Arguments:
+
+        -h/--help   Help
+        -l/--level  Log level [Default: FATAL] 
+            OFF
+            FATAL
+            ERROR
+            WARN
+            INFO
+            DEBUG
+            TRACE
+            ALL
+
+=head1 AUTHOR
+
+Daniel Guariz Pinheiro E<lt>dgpinheiro@gmail.comE<gt>
+
+Copyright (C) 2019 Universidade Estadual Paulista "Júlio de Mesquita Filho"
+
+=head1 LICENSE
+
+GNU General Public License
+
+http://www.gnu.org/copyleft/gpl.html
+
+
+=cut
+
+use strict;
+use warnings;
+use Readonly;
+use Getopt::Long;
+
+use constant MERGED_TAB_FILE_PATH=>"/usr/local/bioinfo/Krona/KronaTools/taxonomy/merged.tab";
+
+
+use vars qw/$LOGGER/;
+
+INIT {
+    use Log::Log4perl qw/:easy/;
+    Log::Log4perl->easy_init($FATAL);
+    $LOGGER = Log::Log4perl->get_logger($0);
+}
+
+my ($level, $mfile, $infile);
+
+#Usage("Too few arguments") if $#ARGV < 0;
+GetOptions( "h|?|help" => sub { &Usage(); },
+            "l|level=s"=> \$level,
+            "m|mfile=s"=>\$mfile,
+            "i|infile=s"=>\$infile
+    ) or &Usage();
+
+use FileHandle;    
+use POSIX 'isatty';
+use Storable;
+
+if ($level) {
+    my %LEVEL = (   
+    'OFF'   =>$OFF,
+    'FATAL' =>$FATAL,
+    'ERROR' =>$ERROR,
+    'WARN'  =>$WARN,
+    'INFO'  =>$INFO,
+    'DEBUG' =>$DEBUG,
+    'TRACE' =>$TRACE,
+    'ALL'   =>$ALL);
+    $LOGGER->logdie("Wrong log level ($level). Choose one of: ".join(', ', keys %LEVEL)) unless (exists $LEVEL{$level});
+    Log::Log4perl->easy_init($LEVEL{$level});
+}
+
+$mfile||=MERGED_TAB_FILE_PATH;
+
+my $hr_data;
+
+if (-e "$mfile.dump") {
+    $hr_data = retrieve("$mfile.dump");
+} else {
+    open(MFILE, "<", $mfile) or $LOGGER->logdie($!);
+    while(<MFILE>) {
+        chomp;
+        my ($old, $new) = split(/\t/, $_);
+        $hr_data->{ $old } = $new;
+    }
+    close(MFILE);
+    store $hr_data, "$mfile.dump";
+}
+
+my $fh;
+
+if ($infile) {
+
+    $LOGGER->logdie("Wrong input file ($infile)") unless (-e $infile);
+
+    $fh = FileHandle->new;
+    $fh->open("<$infile");
+
+} else {
+    unless (isatty(*STDIN)) {
+        $fh = \*STDIN;
+    } else {
+        $LOGGER->logdie("Missing input file or STDIN data");
+    }
+}
+
+while(<$fh>) {
+    chomp;
+    my $taxids=$_;
+    $taxids=~s/^\s+//;
+    $taxids=~s/^\s+$//;
+    my @taxids=split(/\s+/, $taxids);
+    foreach my $taxid (@taxids) {
+        $taxid+=0;
+        if (exists $hr_data->{$taxid}) {
+            print $hr_data->{$taxid},"\n";
+        } else {
+            print "$taxid\n";
+        }
+    }        
+}
+
+$fh->close();
+
+# Subroutines
+
+sub Usage {
+    my ($msg) = @_;
+
+    my $mfile||=MERGED_TAB_FILE_PATH;
+
+	Readonly my $USAGE => <<"END_USAGE";
+Daniel Guariz Pinheiro (dgpinheiro\@gmail.com)
+(c)2019 Universidade Estadual Paulista "Júlio de Mesquita Filho"
+
+Usage
+
+        $0	[-h/--help] [-l/--level <LEVEL>]
+
+Argument(s)
+
+        -h      --help      Help
+        -l      --level     Log level [Default: FATAL]
+        -i      --infile    Input file
+        -m      --merged    merged.tab file [Default: $mfile]
+
+END_USAGE
+    print STDERR "\nERR: $msg\n\n" if $msg;
+    print STDERR qq[$0  ] . q[$Revision$] . qq[\n];
+	print STDERR $USAGE;
+    exit(1);
+}
+
