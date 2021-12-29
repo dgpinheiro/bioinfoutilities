8f9cd2e26072d2dd3e54a79e5119f5253aa8a926
dgpinheiro <dgpinheiro@gmail.com>
2021-12-29 18:51:41 -0200

    Added new functionalities

--- run-DESeq2.R
+++ run-DESeq2.R
@@ -19,8 +19,12 @@ if("--help" %in% args) {
       --ncores=Integer		- number of processors available
       --ignore=someValue	- columns to ignore (comma separated values). Example: --ignore=\"id,desc,other\"
       --pseudocount		- number to add
+      --controlgenes		- list of \"control genes\" file (the gene identifiers must be in the input file)
+      --refgroup		- Reference sample group (this group must be in the groups file)
+      --lfcshrink		- LFC shrink (\"apeglm\", \"ashr\", \"normal\")
+							 
       --help            	- print this Help
-
+     
 	GROUP file must be formatted as tab-delimited columns with sample names grouped by group name:
 
 	id		name		group
@@ -31,7 +35,7 @@ if("--help" %in% args) {
 	* <GROUP_NAME> is the name of the group
 
       Example:
-      ./run-DESeq2.R --groups=\"groups.txt\" --in=\"input.txt\" --out=\"./output\" 
+      ./run-DESeq2.R --groups=\"groups.txt\" --in=\"input.txt\" --out=\"./output\" [ --controlgenes=\"control_genes.txt\" --refgroup=\"CTL\" --lfcshrink=\"apeglm\" ]
       
       Daniel Guariz Pinheiro
       FCAV/UNESP - Univ Estadual Paulista
@@ -67,6 +71,27 @@ if(is.null(argsL[['groups']])) {
 	sink()
 	q(save="no")
 }
+controlGenes<-c()
+if( ! is.null(argsL[['controlgenes']])) {
+	if ( ! file.exists(argsL[['controlgenes']]) ) {
+		sink(stderr())
+		cat("\nERROR: List of \"control genes\" file not found!\n\n")
+		sink()
+		q(save="no")
+	}
+	
+	controlGenes <- unique( read.delim(file=argsL[['controlgenes']], header=FALSE,stringsAsFactors=FALSE)$V1 )
+
+}
+
+if ( ! is.null(argsL[['lfcshrink']]) ) {
+	if ( ! argsL[['lfcshrink']] %in% c("apeglm", "ashr", "normal") ) {
+		sink(stderr())
+		cat("\nERROR: Wrong lfcShrink type !\n\n")
+		sink()
+		q(save="no")
+	}
+}
 
 if ( ! file.exists(argsL[['in']]) ) {
 	sink(stderr())
@@ -87,12 +112,18 @@ if ( ! file.exists(argsL[['groups']]) ) {
 	q(save="no")
 }
 
+if(! is.null(argsL[['refgroup']])) {
+	sink(stderr())
+	cat(paste("\nWARNING: Using reference sample group:",argsL[['refgroup']],"\n\n"))
+	sink()
+}
+
 if(is.null(argsL[['ncores']])) {
 	argsL[['ncores']] = 1
 }
 
 if(is.null(argsL[['pseudocount']])) {
-	argsL[['pseudocount']] = 0
+	argsL[['pseudocount']] = 1
 }
 
 ignorecols=c()
@@ -123,6 +154,15 @@ if(is.null(argsL[['groups']])) {
 	g <-data.frame( id=samps, name=samps, group=samps, stringsAsFactors=FALSE )
 } else {
 	g <-read.delim( argsL[['groups']], header=TRUE, stringsAsFactor=FALSE )
+
+	if(! is.null(argsL[['refgroup']])) {
+		if (! any(g$group==argsL[['refgroup']])) {
+			sink(stderr())
+			cat(paste("\nERROR: Not found reference group (",argsL[['refgroup']], ") in groups file (",argsL[['groups']],") !\n\n", sep=""))
+			sink()
+			q(save="no")
+		}
+	}
 }
 
 if (any(!colnames(g) %in% c('id', 'name', 'group'))) {
@@ -142,13 +182,7 @@ if ( any(! ( g$id %in% samps )) ) {
 
 rownames(x) <- x[[idcol]]
 
-# Low abundance filtering
-#cat("Filtering low abundance reads...\n")
-#keep <- rowSums(cpm(x[,samps])>1) >= 5
-#x.filtered <- x[keep, ]
-x.filtered <- x
-
-countData <- x.filtered[ , as.character(g$id)]
+countData <- x[ , as.character(g$id)]
 
 rownames(g) <- g$id
 
@@ -167,66 +201,84 @@ dds <- DESeqDataSetFromMatrix(countData = countData+as.numeric(argsL[['pseudocou
                               colData = colData,
                               design = ~ group)
 
-dds <- estimateSizeFactors(dds)
+if (length(controlGenes) > 0) {
+	
+	sink(stderr())
+	cat(paste("\nWARNING: Estimating size factor using control genes: ",paste(controlGenes,collapse=", "),"\n\n"))
+	sink()
+
+	isControl <- rownames(dds) %in% controlGenes
+	
+	dds <- estimateSizeFactors(dds, controlGenes=which(isControl) )
+} else {
+	dds <- estimateSizeFactors(dds)
+}	
 sizeFactors(dds)
+dds <- estimateDispersions(dds) 
 
-if (is.na(tryCatch( 
-		dds <- estimateDispersions(dds,fitType="parametric") 
-		  , 
-		  error=function(e) NA 
-		  ) )) {
-		dds <- estimateDispersionsGeneEst(dds)
-		dispersions(dds) <- mcols(dds)$dispGeneEst
-}
-
-cat("DESeq2...\n")
+#if (is.na(tryCatch( 
+#		dds <- estimateDispersions(dds) 
+#		  , 
+#		  error=function(e) NA 
+#		  ) )) {
+#		dds <- estimateDispersionsGeneEst(dds)
+#		dispersions(dds) <- mcols(dds)$dispGeneEst
+#}
+#
+#cat("DESeq2...\n")
 #dds <- DESeq(dds, parallel=TRUE)
 dds = nbinomWaldTest(dds)
 
 deseq_ncounts <- as.data.frame( counts(dds, normalized=TRUE) )
 deseq_ncounts[[idcol]] <- rownames(deseq_ncounts)
 
+mean.cols <- c()
+for (lvl in levels(colData$group) ) {
+	deseq_ncounts[[paste(lvl,'mean',sep=".")]] <- rowMeans(deseq_ncounts[, g[g$group==lvl, 'name'] ])
+	mean.cols <- c(mean.cols, paste(lvl,'mean',sep="."))
+}
 
 design <- model.matrix(~0+group, data=colData)
 colnames(design) <- levels(colData$group)
 
 
-all.comb <- apply(t(combn(levels(colData$group),2)), 1, function(x) { paste( x ,collapse="-") })
+if(is.null(argsL[['refgroup']])) {
+	all.comb <- apply(t(combn(levels(colData$group),2)), 1, function(x) { paste( x ,collapse="-") })
+} else {
+	all.comb <- paste(setdiff(levels(colData$group), argsL[['refgroup']]), argsL[['refgroup']], sep="-")
+}
 
 cat("Contrast results...\n")
 res <- list()
 for (c in 1:length(all.comb)) {
+	
 	print(all.comb[c])
-	#my.contrast <- makeContrasts(contrasts=all.comb[c], levels=design)
+
 	one <- unlist(strsplit(all.comb[c],'-'))[1]
 	two <- unlist(strsplit(all.comb[c],'-'))[2]
-	#save(list = ls(all=TRUE,envir=globalenv()), file = "run-DESeq2.RData")
 	
-	res[[ all.comb[c] ]] <- results(dds, contrast=c("group",one,two))
+	if ( is.null(argsL[['lfcshrink']]) ) {
+		res[[ all.comb[c] ]] <- results(dds, contrast=c("group",one,two))
+		dgeres.df <- as.data.frame(  res[[ all.comb[c] ]]  )
+		dgeres.df[[idcol]] <- rownames(dgeres.df)
 
-	dgeres.df <- as.data.frame(  res[[ all.comb[c] ]]  );
-	dgeres.df[[idcol]] <- rownames(dgeres.df)
+		rownames(dgeres.df) <- dgeres.df[[idcol]]
+		colnames(dgeres.df) <- c("baseMean","logFC","lfcSE","stat","PValue", "FDR", idcol)
+	} else {
+		res[[ all.comb[c] ]] <- lfcShrink(dds, contrast=c("group",one,two), type=argsL[['lfcshrink']])
+		dgeres.df <- as.data.frame(  res[[ all.comb[c] ]]  )
+		dgeres.df[[idcol]] <- rownames(dgeres.df)
 
-	rownames(dgeres.df) <- dgeres.df[[idcol]]
-	colnames(dgeres.df) <- c("baseMean","logFC","lfcSE","stat","PValue", "FDR", idcol)
-	
-	dge.res <- (merge( dgeres.df, deseq_ncounts, by=idcol))[,c(idcol, g$name, 'logFC', 'PValue', 'FDR')]
+		rownames(dgeres.df) <- dgeres.df[[idcol]]
+		colnames(dgeres.df) <- c("baseMean","logFC","lfcSE","PValue", "FDR", idcol)
+	}
+
+	dge.res <- (merge( dgeres.df, deseq_ncounts, by=idcol))[,c(idcol, g$name, mean.cols, 'logFC', 'lfcSE', 'PValue', 'FDR')]
 	rownames(dge.res) <- dge.res[[idcol]]
 		
-	samps.reordered <- c()
-	for (l in levels(colData$group)) { 
-		#print(l) 
-		samps.reordered <- c(samps.reordered, g[which(g$group==l), 'name'])
-	}
-
-	for (col in ignorecols) {
-		dge.res[[col]] <- x.filtered[rownames(dge.res), col]
-	}
-
-	dge.res$FDR <- p.adjust(dge.res$PValue, method="fdr")
-
 	write.table(file=paste(argsL[['out']],"/",all.comb[c],".txt",sep=""), x=dge.res, row.names=FALSE, quote=FALSE, sep="\t")
+
 }
 
-#save(list = ls(all=TRUE,envir=globalenv()), file = "run-DESeq2.RData")
+save(list = ls(all=TRUE,envir=globalenv()), file = paste(argsL[['out']],"/","run-DESeq2.RData", sep=""))
 
